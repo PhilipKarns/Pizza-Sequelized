0b2a810d1a3b15abaff71a939b515dd5a0221175
Karns <pkarns@deloitte.com>
2017-04-29 15:36:55 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+//create server and set default port
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+//handlebars boilerplate
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/pizza_controller.js");
+
+app.use("/", routes);
+
+//listener for port connection
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
