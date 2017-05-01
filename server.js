d3cec321ac3071918a769e1f8e67b6ad2aa2d315
Karns <pkarns@deloitte.com>
2017-05-01 11:08:14 -0400

    update to server.js

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ var db = require("./models");
 app.use(express.static(process.cwd() + "/public"));
 
 // USED THIS THE FIRST TIME. REMOVE?Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
